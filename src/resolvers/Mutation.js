e0d56061b516bd7c812d9199dc07e7ddb7833e6f
r-firdovsi <r-firdovsi@hotmail.com>
2019-10-23 01:03:08 +0400

    I make Update Mutations for Post, User and Comment

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -18,6 +18,34 @@ const Mutation =  {
         return user;
 
     },
+    updateUser(parent, args, { db }, info) {
+        const {id, data} = args;
+        const user = db.users.find((user) => user.id == id);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        if (typeof data.email === 'string') {
+            const emailTaken = db.users.some((user) => user.email === data.email);
+
+            if (emailTaken) {
+                throw new Error('Email taken')
+            }
+
+            user.email = data.email;
+        }
+
+        if (typeof data.name === 'string') {
+            user.name = data.name;
+        }
+
+        if (typeof data.age !== 'undefined') {
+            user.age = data.age
+        }
+
+        return user
+    },
     deleteUser(params, args, {db}, info) {
         const userIndex = db.users.findIndex((user) => user.id === args.id);
 
@@ -56,6 +84,28 @@ const Mutation =  {
         return post;
 
     },
+    updatePost(parent, args, {db}, info) {
+        const {id, data} = args;
+        const post = db.posts.find((post) => post.id === id);
+
+        if (!post) {
+            throw new Error('Post not found');
+        }
+
+        if (typeof data.title == 'string') {
+            post.title = data.title
+        }
+
+        if (typeof data.body == 'string') {
+            post.body = data.body
+        }
+
+        if (typeof data.published !== 'undefined') {
+            post.published = data.published;
+        }
+
+        return post
+    },
     deletePost(parent, args, {db}, info) {
         const postIndex = db.posts.findIndex((post) => post.id === args.id);
 
@@ -83,6 +133,20 @@ const Mutation =  {
         };
 
         db.comments.push(comment);
+
+        return comment;
+    },
+    updateComment(parent, args, {db}, info) {
+        const {id, data} = args;
+        const comment = db.comments.find((comment) => comment.id == id);
+
+        if (!comment) {
+            throw new Error('Comment not found');
+        }
+
+        if (typeof data.text == 'string') {
+            comment.text = data.text
+        }
 
         return comment;
     },
